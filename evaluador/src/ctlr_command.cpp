777c37ba53a7b16be5aa2665dbae300ebc134bfc
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-06-08 18:31:22 -0500

    Updating ctlr_command.cpp

--- evaluador/src/ctlr_command.cpp
+++ evaluador/src/ctlr_command.cpp
@@ -1,46 +1,25 @@
 #include "ctlr_command.h"
 
 void command_ctlr(char* commands[], int* length){
-    char* type;
-    string i;
-    int d = 100;
+    char* interactive_commands[2];
+    bool isN = false;
     char *n = (char*)"evaluator";
     if (*length == 2){
-        cerr<<"missing -n command\n";
+    }
+    else if(*length%2 != 0){
+        cerr<<"command invalid\n";
         exit(EXIT_FAILURE);
     }else{
         for(int it=2; it<*length; it+=2){
             if(strcmp(commands[it],"-n")==0){
-                    n = commands[it+1];
-                    if(strcmp(commands[it+2], "list")==0){
-                        if(strcmp(commands[it+3], "processing")==0){
-                            cout << "Processing:\n";
-                        }else if(strcmp(commands[it+3], "waiting")==0){
-                            cout << "Waiting:\n";
-                        }else if(strcmp(commands[it+3], "reported")==0){
-                            cout << "Reported:\n";
-                        }else if(strcmp(commands[it+3], "reactive")==0){
-                            cout << d << endl;
-                        }else if(strcmp(commands[it+3], "all")==0){
-                            cout << "Processing:\n";
-                            cout << "Waiting:\n";
-                            cout << "Reported:\n";
-                            cout << d << endl;
-                        }else{
-                            cerr<<"invalid subcommand";
-                            exit(EXIT_FAILURE);
-                        }
-                    }else if(strcmp(commands[it+2], "update")==0){
-                        if(strcmp(commands[it+3], "B") || strcmp(commands[it+3], "S") || strcmp(commands[it+3], "B")==0){
-                            type = commands[it+4];
-                        }else{
-                            cerr<<"expected B|D|S";
-                            exit(EXIT_FAILURE);
-                        }
-                    }else{
-                        cout << "invalid subcommand" << endl;
-                    }
+                n = commands[it+1]; 
+                isN = true;
             }
         }
     }
+    if(isN){
+       // Do subcommands
+    }
+    exit(EXIT_SUCCESS);
 }
+
