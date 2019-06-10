e24714fa9396a696640167b91274beede3514fa0
root <root@LAPTOP-SRNPULR2.localdomain>
2019-06-09 22:55:31 -0500

    Init test

--- evaluador/include/thread_info.h
+++ evaluador/include/thread_info.h
@@ -4,7 +4,13 @@ struct Input_info
 {
     int input;
     void* init_dir;
-    sem_t *semaforo;    
-    Input_info(int input,void* init_dir,sem_t *semaforo):input(input),init_dir(init_dir),semaforo(semaforo){   }
+    sem_t *mutex;
+    sem_t *full;
+    sem_t *empty;
+    sem_t **b;
+    sem_t **d;
+    sem_t **s;     
+    Input_info(int input,void* init_dir,sem_t *mutex,sem_t *full, sem_t *empty, sem_t **b, sem_t **d, sem_t **s)
+    :input(input),init_dir(init_dir),mutex(mutex),full(full),empty(empty),b(b),d(d),s(s){    }
 };
 
