e2dd08e5a9c690e4cc0e9f039c3554c00dbb22c9
root <root@LAPTOP-SRNPULR2.localdomain>
2019-06-10 03:09:50 -0500

    Pro int

--- evaluador/include/share_memory.h
+++ evaluador/include/share_memory.h
@@ -4,12 +4,16 @@
 
 using namespace std;
 
-struct exam{
-    int id,pila;
+
+struct exam  {
+    int id, _queue, _quant, _state;
     char type;
 };
 
 struct head{
-    int i,ie,oe,b,d,s,q, first_exit, end_exit, first_b, end_b, first_d, end_d, first_s, end_s;
+    int i, ie ,oe ,b ,d, s , q, 
+        first_exit, end_exit, first_b, 
+        end_b, first_d, end_d, first_s, end_s, _uuid, stop,
+        qua_o,qua_b,qua_d,qua_s;
     char n[30];
 };
--- evaluador/src/init_command.cpp
+++ evaluador/src/init_command.cpp
@@ -17,7 +17,7 @@ static void* input_thread(void * info);
 
 void command_init(char* commands[], int length){
     int i=5, ie=6, oe=10, b=100, d=100, s=100, q=6;
-    string n = (char *)"evaluator";
+    string n = "evaluator";
     if (length == 2){
     }
     else if(length%2 != 0){
@@ -80,6 +80,7 @@ void command_init(char* commands[], int length){
             }
         }
     }
+
     //Create the shared memory  and initialice all the values
     int fd = shm_open(n.c_str(), O_RDWR | O_CREAT | O_EXCL, 0660);
     if (fd < 0) {
@@ -106,6 +107,7 @@ void command_init(char* commands[], int length){
     }
 
     head *pHead = (head * )dir;
+    pHead->_uuid = 0;
     pHead->i = i;
     pHead->ie = ie;
     pHead->oe = oe;
@@ -121,27 +123,35 @@ void command_init(char* commands[], int length){
     pHead->end_d = 0;
     pHead->first_s = 0;
     pHead->end_s = 0;
+    pHead->stop = true;
 
     pthread_t* inputs_threads = new pthread_t[i];
 
-    sem_t *_b[3] = new sem_t*[3];
-    b[0] = sem_open(n+"_inter_b_mutex",O_CREAT | O_EXCL, 0660, 1);
-    b[1] = sem_open(n+"_inter_b_empty",O_CREAT | O_EXCL, 0660, 1);
-    b[2] = sem_open(n+"_inter_b_full" ,O_CREAT | O_EXCL, 0660, 1);
-    sem_t *_d[3] = new sem_t*[3];
-    sem_t *_s[3] = new sem_t*[3];
+    sem_t **_b = new sem_t*[3];
+    _b[0] = sem_open((n+"_inter_b_mutex").c_str() ,O_CREAT | O_EXCL, 0660, 1);
+    _b[1] = sem_open((n+"_inter_b_empty").c_str() ,O_CREAT | O_EXCL, 0660, q);
+    _b[2] = sem_open((n+"_inter_b_full" ).c_str() ,O_CREAT | O_EXCL, 0660, 0);
+    sem_t **_d = new sem_t*[3];
+    _d[0] = sem_open((n+"_inter_d_mutex").c_str() ,O_CREAT | O_EXCL, 0660, 1);
+    _d[1] = sem_open((n+"_inter_d_empty").c_str() ,O_CREAT | O_EXCL, 0660, q);
+    _d[2] = sem_open((n+"_inter_d_full").c_str()  ,O_CREAT | O_EXCL, 0660, 0);
+    
+    sem_t **_s = new sem_t*[3];
+    _s[0] = sem_open((n+"_inter_s_mutex").c_str(),O_CREAT | O_EXCL, 0660, 1);
+    _s[1] = sem_open((n+"_inter_s_empty").c_str(),O_CREAT | O_EXCL, 0660, q);
+    _s[2] = sem_open((n+"_inter_s_full").c_str() ,O_CREAT | O_EXCL, 0660, 0);
     
     for(int it = 0; it<i; it++){
         string sem_name_mutex = n + "_input_" + to_string(it).c_str() + "_mutex";
         string sem_name_empty = n + "_input_" + to_string(it).c_str() + "_empty";
-        string sem_name_full  = n + "_input_" + to_string(it).c_str() + "_full"; 
+        string sem_name_full  = n + "_input_" + to_string(it).c_str() + "_fulls"; 
         sem_t *mutex = sem_open(sem_name_mutex.c_str(), O_CREAT | O_EXCL, 0660,1); 
         sem_t *empty = sem_open(sem_name_empty.c_str(), O_CREAT | O_EXCL, 0660, ie);
         sem_t *full =  sem_open(sem_name_full.c_str(),  O_CREAT | O_EXCL, 0660, 0);
         Input_info* info = new Input_info(it , dir , mutex , full , empty , _b , _d , _s);
         pthread_create(&inputs_threads[it],NULL,input_thread,(void *) info);
-        phtread_join(input_thread[it],NULL);
     }
+    pthread_join(inputs_threads[0],NULL);
 }
 
 static void* input_thread(void *input){
@@ -152,30 +162,59 @@ static void* input_thread(void *input){
     head* phead = (head *)init_pos;
     int input_number = info->input;
     int length_array = phead->ie;
-    int *last  = (int *)(init_pos + head_size + length_array*(phead->i)*exam_size+3*(phead->q) + phead->i*sizeof(int) + input_number*sizeof(int));
+    int *last  = (int *)(init_pos + head_size + length_array*(phead->i)*exam_size + phead->oe*exam_size + 3*(phead->q)*exam_size + phead->i*sizeof(int) + input_number*sizeof(int));
+    int *qua  = (int *)(init_pos + head_size + length_array*(phead->i)*exam_size  + phead->oe*exam_size + 3*(phead->q)*exam_size + 2*(phead->i*sizeof(int)) + input_number*sizeof(int));
     sem_t *mutex = info->mutex;
     sem_t *empty = info->empty;
     sem_t *full  = info->full;
+    sem_t** b = info->b;
+    sem_t** d = info->d;
+    sem_t** s = info->s;
     char *input_array = init_pos + head_size + input_number*length_array*exam_size;
+    char *dir_b = (char *)(init_pos + head_size + length_array*(phead->i)*exam_size + phead->oe*exam_size);
+    char *dir_d = (char *)(init_pos + head_size + length_array*(phead->i)*exam_size + phead->oe*exam_size + (phead->q)*exam_size);
+    char *dir_s = (char *)(init_pos + head_size + length_array*(phead->i)*exam_size + phead->oe*exam_size + 2*(phead->q)*exam_size);
     exam current_exam;
-    for(;;){
+    exam *pinter_q;
+    while(!phead->stop){
         sem_wait(full);
         sem_wait(mutex);
         current_exam = *((exam *) (input_array + *last*exam_size));
-        cout<<"Pila "<<current_exam.id<<endl;
+        *last = (*last + 1) % length_array;
+        *qua = *qua-1;
         char type = current_exam.type;
-        if(type == 'b'){
-
+        if(type == 'B'){
+            sem_wait(b[1]);
+            sem_wait(b[0]);
+            pinter_q = (exam *)(dir_b + (phead->end_b)*exam_size);
+            *pinter_q = current_exam;
+            phead->end_b = (phead->end_b + 1)%(phead->q);
+            phead->qua_b++; 
+            sem_post(b[0]);
+            sem_post(b[2]);
         }
-        else if(type == 'd'){
-
+        else if(type == 'D'){
+            sem_wait(d[1]);
+            sem_wait(d[0]);
+            pinter_q = (exam *)(dir_d + (phead->end_d)*exam_size);
+            *pinter_q = current_exam;
+            phead->end_d = (phead->end_d + 1)%(phead->q);
+            phead->qua_d++;
+            sem_post(d[0]);
+            sem_post(d[2]);
         }
-        else if(type == 's'){
-
+        else if(type == 'S'){
+            sem_wait(s[1]);
+            sem_wait(s[0]);
+            pinter_q = (exam *)(dir_s + (phead->end_s)*exam_size);
+            *pinter_q = current_exam;
+            phead->end_s = (phead->end_s + 1)%(phead->q);
+            phead->qua_s++;
+            sem_post(s[0]);
+            sem_post(s[2]);
         }
         sem_post(mutex);
         sem_post(empty);
     }
-
     return NULL;
 }   
